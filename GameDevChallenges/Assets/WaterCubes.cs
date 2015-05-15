ec43b471b54465e5976757b97cda9715ca836d70
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-15 08:45:05 -0500

    / ‘GameDevChallenges/Assets/WaterCubes.cs’

--- GameDevChallenges/Assets/WaterCubes.cs
+++ GameDevChallenges/Assets/WaterCubes.cs
@@ -201,7 +201,7 @@ public class WaterCubes : MonoBehaviour {
 					int _z = z;
 					left_height = height_of (_x, _z);
 					next.l = flux (current.l, height - left_height);
-					Debug.Log ("LEFT OUTFLOW: "+next.l);
+//					Debug.Log ("LEFT OUTFLOW: "+next.l);
 
 				}
 				if (x <= size - 2) {
@@ -210,7 +210,7 @@ public class WaterCubes : MonoBehaviour {
 					int _z = z;
 					right_height = height_of (_x, _z);
 					next.r = flux (current.r, height - right_height);
-					Debug.Log ("RIGHT OUTFLOW: "+next.r);
+//					Debug.Log ("RIGHT OUTFLOW: "+next.r);
 				}
 				if (z >= 1) {
 					//bottom neighbor eligible
@@ -218,7 +218,7 @@ public class WaterCubes : MonoBehaviour {
 					int _z = z - 1;
 					bottom_height = height_of (_x, _z);
 					next.b = flux (current.b, height - bottom_height);
-					Debug.Log ("BOTTOM OUTFLOW: "+next.b);
+//					Debug.Log ("BOTTOM OUTFLOW: "+next.b);
 				}
 				if (z <= size - 2) {
 					//top neighbor eligible
@@ -226,7 +226,7 @@ public class WaterCubes : MonoBehaviour {
 					int _z = z + 1;
 					top_height = height_of (_x, _z);
 					next.t = flux (current.t, height - top_height);
-					Debug.Log ("TOP OUTFLOW: "+next.t);
+//					Debug.Log ("TOP OUTFLOW: "+next.t);
 				}
 
 //				float sum = next.l + next.r + next.t + next.b;
