041dd5e1a65c6b211b4141103b040df77c1adfdb
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-15 11:39:06 -0500

    / ‘GameDevChallenges/Assets/WaterCubes.cs’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb’ / ‘GameDevChallenges/Library/assetDatabase3’ / ‘GameDevChallenges/Library/metadata/f2/f2132b71ebe9549469032b8934b41c1c’ + ‘GameDevChallenges/Temp/UnityTempFile-08694b3fd4ce44081ac54689e3e43afa’ + ‘GameDevChallenges/Temp/UnityTempFile-37fac503fb2204cfa9dfe4bf60d4cf21’

--- GameDevChallenges/Assets/WaterCubes.cs
+++ GameDevChallenges/Assets/WaterCubes.cs
@@ -65,7 +65,7 @@ public class WaterCubes : MonoBehaviour {
 	}
 
 	private float flux(float flux, float heightDifference) {
-		Debug.Log (flux + "+" + heightDifference + "*" + fluxMultiplier + "=" + (flux + heightDifference * fluxMultiplier));
+//		Debug.Log (flux + "+" + heightDifference + "*" + fluxMultiplier + "=" + (flux + heightDifference * fluxMultiplier));
 		return Mathf.Max (0, flux + heightDifference*fluxMultiplier);
 	}
 
