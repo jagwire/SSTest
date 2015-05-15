fad2e7df15f1a9385ad8a89e51ee5dea64fa3c07
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-15 08:47:38 -0500

    / ‘GameDevChallenges/Assets/WaterCubes.cs’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb’ / ‘GameDevChallenges/Library/assetDatabase3’ / ‘GameDevChallenges/Library/metadata/f2/f2132b71ebe9549469032b8934b41c1c’ + ‘GameDevChallenges/Temp/UnityTempFile-e78c4fb7145484edcbbf048db75849ff’ + ‘GameDevChallenges/Temp/UnityTempFile-fd77da0294ad04bfcadce947f98f0f33’

--- GameDevChallenges/Assets/WaterCubes.cs
+++ GameDevChallenges/Assets/WaterCubes.cs
@@ -303,9 +303,8 @@ public class WaterCubes : MonoBehaviour {
 					t_flow_in = current_flux [indexOf (_x, _z)].b;
 				}
 				changesInVolume [indexOf (x, z)] = deltaTime * ((l_flow_in + r_flow_in + t_flow_in + b_flow_in) - (outflow.l + outflow.r + outflow.t + outflow.b));
-
-				Debug.Log ("CHANGE IN VOLUME FOR: "+x+","+z+"="+changesInVolume[indexOf(x,z)]+"| D2 = "+D2[indexOf (x,z)]);
 				D2[indexOf (x,z)] = D1[indexOf (x,z)] + changesInVolume[indexOf (x,z)];
+				Debug.Log ("CHANGE IN VOLUME FOR: "+x+","+z+"="+changesInVolume[indexOf(x,z)]+"| D2 = "+D2[indexOf (x,z)]);
 			}
 		}
 	}
