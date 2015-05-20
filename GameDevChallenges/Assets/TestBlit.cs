93878f618dd35f53784607259f4c90b8753f3129
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-20 10:31:42 -0500

    / ‘GameDevChallenges/Assembly-CSharp-vs.csproj’ / ‘GameDevChallenges/Assembly-CSharp.csproj’ + ‘GameDevChallenges/Assets/TestBlit.cs’ + ‘GameDevChallenges/Assets/TestBlit.cs.meta’ / ‘GameDevChallenges/GameDevChallenges-csharp.sln’ / ‘GameDevChallenges/GameDevChallenges.sln’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb’ / ‘GameDevChallenges/Library/assetDatabase3’ / ‘GameDevChallenges/Library/guidmapper’ ...

---
+++ GameDevChallenges/Assets/TestBlit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBlit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
