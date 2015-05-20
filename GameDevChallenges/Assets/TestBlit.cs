41726988961e44ec565495de76e8e533ffe4b422
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-20 10:41:50 -0500

    / ‘GameDevChallenges/Assets/TestBlit.cs’

--- GameDevChallenges/Assets/TestBlit.cs
+++ GameDevChallenges/Assets/TestBlit.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class TestBlit : MonoBehaviour {
+		
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public Material kernel;
+	public int size;
 	// Update is called once per frame
 	void Update () {
-	
+		Texture2D t = new Texture2D (size, size);
+		RenderTexture rt = new RenderTexture (size, size, 16, RenderTextureFormat.ARGBFloat);
+
+		Graphics.Blit (t, rt, kernel, -1);
 	}
 }
