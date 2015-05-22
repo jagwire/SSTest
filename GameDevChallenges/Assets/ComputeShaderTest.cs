0c09d0b0517ef91c78a39666c9d3b0d59847de6f
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-22 09:07:27 -0500

    / ‘GameDevChallenges/Assets/ComputeShaderTest.cs’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb’ / ‘GameDevChallenges/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb’ / ‘GameDevChallenges/Library/assetDatabase3’ / ‘GameDevChallenges/Library/metadata/1f/1fdb11849d1b84d5ebf14678dff4e33d’ + ‘GameDevChallenges/Temp/UnityTempFile-24e55ab8d8d2544cf89fb37697cff2cf’ + ‘GameDevChallenges/Temp/UnityTempFile-fb0e863a6fcad42b59386c6a9c802dba’

--- GameDevChallenges/Assets/ComputeShaderTest.cs
+++ GameDevChallenges/Assets/ComputeShaderTest.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class ComputeShaderTest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Awake() {
+		Debug.LogError("SUPPORTS SHADERS? -> "+SystemInfo.supportsComputeShaders);	
 	}
 }
