ad1bfd1511426ee7f5da5a58b234e4ebd9124b81
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-22 14:39:58 -0500

    / ‘GameDevChallenges/Assets/TestBlit.cs’

--- GameDevChallenges/Assets/TestBlit.cs
+++ GameDevChallenges/Assets/TestBlit.cs
@@ -7,9 +7,18 @@ public class TestBlit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Texture2D t = new Texture2D (size, size);
+		
+		RenderTexture _rt = 
+		
+		
+		
 		RenderTexture rt = new RenderTexture (size, size, 16, RenderTextureFormat.ARGBFloat);
 
 		Graphics.Blit (t, rt, kernel, -1);
 	
+	}
+	
+	RenderTexture newTexture() {
+		return RenderTexture.GetTemporary(size, size,24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
 	}
 }
