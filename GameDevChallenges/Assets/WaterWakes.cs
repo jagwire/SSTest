59fcf1cae9f79e6e8ccb923d70b51105ca2655bf
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-26 14:45:05 -0500

    / ‘GameDevChallenges/Assets/WaterWakes.cs’

--- GameDevChallenges/Assets/WaterWakes.cs
+++ GameDevChallenges/Assets/WaterWakes.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class WaterWakes : MonoBehaviour {
 
+	Mesh waterMesh;
+	MeshFilter waterMeshFilter;
+	float waterWidth = 3f;
+	float gridSpacing = 0.1f;
+
 	// Use this for initialization
 	void Start () {
-	
+		waterMeshFilter - this.GetComponent<MeshFilter>();
+		List<Vector3[]> height_tmp = GenerateWaterMesh.GenerateWater(waterMeshFilter, waterWidth, gridSpacing);	
 	}
 	
 	// Update is called once per frame
