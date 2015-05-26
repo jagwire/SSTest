49a533d48fd90632dca6746ba74af0f0b0a6a192
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-26 15:07:06 -0500

    / ‘GameDevChallenges/Assets/WaterWakes.cs’

--- GameDevChallenges/Assets/WaterWakes.cs
+++ GameDevChallenges/Assets/WaterWakes.cs
@@ -82,7 +82,46 @@ public class WaterWakes : MonoBehaviour {
 		
 		if(x <= 3.0f) { Debug.Log("Smaller"); }
 		
+		if (x <= 3f) {
+		//Ignored the small rest term at the end
+		J_zero_of_X = 
+			1f - 
+				2.2499997f * Mathf.Pow (x / 3f, 2f) + 
+				1.2656208f * Mathf.Pow (x / 3f, 4f) -
+				0.3163866f * Mathf.Pow (x / 3f, 6f) +
+				0.0444479f * Mathf.Pow (x / 3f, 8f) - 
+				0.0039444f * Mathf.Pow (x / 3f, 10f)+
+				0.0002100f * Mathf.Pow (x / 3f, 12f);
+	}
+	//9.4.3
+	//3 <= x <= infinity
+	else {
+		//Ignored the small rest term at the end
+		float f_zero = 
+			0.79788456f -
+				0.00000077f * Mathf.Pow (3f / x, 1f) -
+				0.00552740f * Mathf.Pow (3f / x, 2f) -
+				0.00009512f * Mathf.Pow (3f / x, 3f) -
+				0.00137237f * Mathf.Pow (3f / x, 4f) -
+				0.00072805f * Mathf.Pow (3f / x, 5f) + 
+				0.00014476f * Mathf.Pow (3f / x, 6f);
 		
+		//Ignored the small rest term at the end
+		float theta_zero = 
+			x -
+				0.78539816f - 
+				0.04166397f * Mathf.Pow (3f / x, 1f) -
+				0.00003954f * Mathf.Pow (3f / x, 2f) -
+				0.00262573f * Mathf.Pow (3f / x, 3f) -
+				0.00054125f * Mathf.Pow (3f / x, 4f) -
+				0.00029333f * Mathf.Pow (3f / x, 5f) + 
+				0.00013558f * Mathf.Pow (3f / x, 6f);
+		
+		//Should be cos and not acos
+		J_zero_of_X = Mathf.Pow(x, -1f/3f) * f_zero * Mathf.Cos(theta_zero);
+	}
+	
+	return J_zero_of_X;
 	}
 	
 	
