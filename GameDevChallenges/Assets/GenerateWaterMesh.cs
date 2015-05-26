e93b376570bfebb4797e00f1b67fb565ee4497a5
Ryan Babiuch <pympnotiq@gmail.com>
2015-05-26 14:50:01 -0500

    / ‘GameDevChallenges/Assets/GenerateWaterMesh.cs’

--- GameDevChallenges/Assets/GenerateWaterMesh.cs
+++ GameDevChallenges/Assets/GenerateWaterMesh.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GenerateWaterMesh : MonoBehaviour {
 	public static List<Vector3[]> GenerateWater(MeshFilter waterMeshFilter, float size, float spacing) {
+		int totalVertices = (int)Mathf.Round(size/spacing) + 1;
+		
+		List<Vector3[]> vertices2dArray = new List<Vector3[]>();
 		
 	}
 }
